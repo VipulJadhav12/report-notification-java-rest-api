a563ed2afd64c67463fbd09acc42e10ebac24f82
VipulJadhav12 <vipulsjadhav.12@gmail.com>
2022-12-18 18:07:00 +0530

    Initial commit. Added raw Spring Boot v2.7.6 project.

---
+++ src/main/java/com/afourathon/report_notification_rest_api/ReportNotificationRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.afourathon.report_notification_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportNotificationRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportNotificationRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afourathon/report_notification_rest_api/ReportNotificationRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afourathon.report_notification_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportNotificationRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
