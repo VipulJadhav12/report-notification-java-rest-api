90067871df77f2abdc2648e6f33de5a0ba27fcc6
VipulJadhav12 <vipulsjadhav.12@gmail.com>
2023-01-09 11:01:05 +0530

    Added centralized logging using Spring Boot AOP.

--- src/main/java/com/afourathon/report_notification_rest_api/configuration/Configurations.java
+++ src/main/java/com/afourathon/report_notification_rest_api/configuration/Configurations.java
@@ -1,7 +1,9 @@
 package com.afourathon.report_notification_rest_api.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,24 +11,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class Configurations implements WebMvcConfigurer {
-	
+
 	public static final String INVALID_PROJECT_ID = "Invalid Project ID. Project doesn't exists with ID: %d.";
-		
+
 	public static final String INVALID_WEEKLY_STATUS_ID = "Invalid Weekly-Status ID. Weekly-Status doesn't exists with ID: %d";
-	
+
 	public static final String EMAIL_SUBJECT = "Weekly-Status Report of the Project: %s as of: %s";
-	
+
 	public static final String EMAIL_NOTIFICATION_ON_SUCCESS = "Unable to send an email report for the Weekly-Status ID: %d from the Project ID: %d.";
-			
+
 	public static final String EMAIL_NOTIFICATION_ON_FAILURE = "An email report for the Weekly-Status ID: %d from the Project ID: %d has been sent successfully.";
-	
+
 	@Value("${cors.origin.url}")
 	private String CORS_ORIGIN_URL;
-	
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		System.out.println(CORS_ORIGIN_URL);
 		registry.addMapping("/**").allowedOrigins(CORS_ORIGIN_URL);
 	}
-	
+
+	// Adding a bean definition to enable HTTP request logging
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter
+		= new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		filter.setAfterMessagePrefix("REQUEST DATA: ");
+		return filter;
+	}
+
 }
